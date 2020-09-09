abcebe5e1b379aecaa7111e18b49801a49d3db5e
Daniel Coy <danielfelipe113@gmail.com>
2020-09-09 17:56:21 -0500

    removed debug text

--- src/lib-components/vue-draggable-form-builder.vue
+++ src/lib-components/vue-draggable-form-builder.vue
@@ -48,10 +48,6 @@
       </span>
     </div>
 
-    <pre>
-      {{currentElements}}
-    </pre>
-
 
     <div class="fixed z-10 inset-0 overflow-y-auto vue-draggable-form-preview-modal" :class="{'absolute pointer-events-none opacity-0 ease-in duration-200': !showPreviewModal, 'ease-out duration-300 opacity-100': showPreviewModal}">
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0" >
